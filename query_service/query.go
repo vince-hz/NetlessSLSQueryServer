09df85c1314f1ed480d07f7fe04b86dcb595affb
xuyunshi <405029644@qq.com>
2022-10-12 13:20:46 +0800

    Update secure file

--- query_service/query.go
+++ query_service/query.go
@@ -1,12 +1,36 @@
 package query_service
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
 	sls "github.com/aliyun/aliyun-log-go-sdk"
 )
 
+type Env = struct {
+	ProjectName     string `form:"ProjectName"`
+	Endpoint        string
+	LogStoreName    string
+	AccessKeyID     string
+	AccessKeySecret string
+}
+
+var (
+	env    = Env{}
+	Client sls.ClientInterface
+)
+
+func init() {
+	file, _ := os.Open("env.json")
+	defer file.Close()
+	byteValue, _ := ioutil.ReadAll(file)
+	json.Unmarshal(byteValue, &env)
+	Client = sls.CreateNormalInterface(env.Endpoint, env.AccessKeyID, env.AccessKeySecret, "")
+}
+
 func LogQuery(request sls.GetLogRequest) (*sls.GetLogsResponse, error, *sls.GetHistogramsResponse, error) {
-	Client = sls.CreateNormalInterface(Endpoint, AccessKeyID, AccessKeySecret, "")
-	h, hError := Client.GetHistograms(ProjectName, LogStoreName, request.Topic, request.From, request.To, request.Query)
-	l, lError := Client.GetLogsV2(ProjectName, LogStoreName, &request)
+	h, hError := Client.GetHistograms(env.ProjectName, env.LogStoreName, request.Topic, request.From, request.To, request.Query)
+	l, lError := Client.GetLogsV2(env.ProjectName, env.LogStoreName, &request)
 	return l, lError, h, hError
 }
