8c5de4121c492e6f3f5be6954a04ca69fe0ba4fa
xuyunshi <405029644@qq.com>
2022-10-12 13:37:28 +0800

    Fix typo

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	router := httpserver.DefaultEngine()
 	router.GET("/logs", query_service.LogHandler)
-	router.GET("/customQuery/Logs", query_service.CustomQueryDownloadLog)
+	router.GET("/customQuery/logs", query_service.CustomQueryDownloadLog)
 	router.GET("/downloadLogs", query_service.DownloadHandler)
 	router.GET("/customQuery/downloadLogs", query_service.CustomQueryDownloadLog)
 	router.Run(":8080")
