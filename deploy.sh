75234f9bc7ace804e09b07a139b78c99674c8c8b
xuyunshi <405029644@qq.com>
2022-10-14 10:19:50 +0800

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 IP=$1
-DOWNLOAD_PATH="/home/sls_server/downloads"
+PORT="8080"
+SERVER_DIR_PATH="/home/sls_server"
+DOWNLOAD_PATH="$SERVER_DIR_PATH/downloads"
 NAME="sls_server_bin"
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $NAME
 scp ./$NAME root@$IP:$DOWNLOAD_PATH
 scp ./env.json root@$IP:$DOWNLOAD_PATH
-ssh root@$IP "cd $DOWNLOAD_PATH && mv $NAME .. && mv env.json .. && cd .. && ./$NAME && ps -ef | grep ./$NAME | awk '{print $2}' | xargs kill -9 && nohup ./bin > sls_server.log 2>&1 & exit"
+ssh -f root@$IP "cd $DOWNLOAD_PATH && mv $NAME .. && mv env.json .. && netstat -tunlp|grep $PORT | awk '{print \$7}' | grep -o '[0-9]*' | xargs -r kill && nohup $SERVER_DIR_PATH/$NAME release > $SERVER_DIR_PATH/sls_server.log 2>&1 &"
 rm $NAME
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"netless/slsquery/httpserver"
 	"netless/slsquery/query_service"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	args := os.Args
+	fmt.Println(args, len(args))
+	if len(args) >= 2 {
+		if args[1] == "release" {
+			gin.SetMode(gin.ReleaseMode)
+		}
+	}
+
 	router := httpserver.DefaultEngine()
 	router.GET("/logs", query_service.LogHandler)
 	router.GET("/customQuery/logs", query_service.CustomQueryLogHandler)
